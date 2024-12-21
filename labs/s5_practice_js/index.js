7039f204ebd7e47b3da26b959db25e920f23d247
QuocTK <tranquocbn@gmail.com>
2024-12-21 23:52:21 +0700

    Cập nhật bài kiểm tra lần 1

---
+++ labs/s5_practice_js/index.js
@@ -0,0 +1,47 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// INPUT: numbers <array>
+// OUTPUT: 55
+function getTotal(numbers) {
+    var total = 0;
+    // code logic here...
+
+    return total;
+}
+
+// INPUT: numbers
+// OUTPUT: 30
+function getTotalEvenNumber(numbers) {
+    var total = 0;
+    // Code logic here...
+
+    return total;
+}
+
+// Khởi tạo list user ở đây
+
+var users = [
+    // khởi tạo các object user... thông tin user xem file exam.txt bài 3
+];
+
+// INPUT: users<array>, birthday<int>
+// OUTPUT: 1 mảng users tìm được bởi birthday truyền vào
+function getUsersByBirthDay(users, birthday) {
+    var usersFound = []; // 1 mảng người dùng khi tìm thấy bởi birthday
+
+    return usersFound;
+}
+
+// INPUT: users<array>, gender<string>
+// OUTPUT: 1 mảng users tìm được bởi gender truyền vào
+function getUsersByGender(users, gender) {
+    var usersFound = []; // 1 mảng người dùng khi tìm thấy bởi gender
+
+    return usersFound;
+}
+
+// INPUT: users<array>, point<int>
+// OUTPUT: void - không xác định. - tham chiếu, loại bỏ user có point < pointDelete
+function deleteUserByPoint(users, pointDelete = 1) {
+    // code logic here
+}
