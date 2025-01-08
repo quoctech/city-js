33e3bacb9cec72285460c9f055ac83197a0f97c6
QuocTK <tranquocbn@gmail.com>
2025-01-08 21:49:37 +0700

    Sửa đổ nội dung comment trong buổi 10

--- labs/s10_scope/index.js
+++ labs/s10_scope/index.js
@@ -12,14 +12,14 @@ var y = 15;
 function random() {
     // Local scope
     var x = Math.floor(Math.random() * 9);
-    var z = 100; // chỉ sử dụng được y trong scope hàm random.
+    var z = 100; // chỉ sử dụng được z trong scope hàm random.
 
     y = 20; // sửa đổi data của y từ 15 lên 20.
     console.log('X trong random function (local scope)', x);
 }
 
 console.log('Trước khi thay đổi y', y);
-random();
+random(); 
 console.log('X (global scope)', x);
-console.log('Sau khi thay đổi y', y); // 20 vì random đã được gọi và đã thay đổi giá trị của y từ 10 lên 20.
+console.log('Sau khi thay đổi y', y); // 20 vì random đã được gọi và đã thay đổi giá trị của y từ 15 lên 20.
 // console.log(z); z is not defined
