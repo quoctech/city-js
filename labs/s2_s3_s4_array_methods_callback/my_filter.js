9c516ecd5fb757409e7b3cd787f5fc25f0224626
Le Theu <theule21098@gmail.com>
2025-01-10 21:56:02 +0700

    upload bai tap s2 my_filter

--- labs/s2_s3_s4_array_methods_callback/my_filter.js
+++ labs/s2_s3_s4_array_methods_callback/my_filter.js
@@ -1,8 +1,8 @@
 var products = [
-    {id: 1, name: 'Iphone 8', price: 700},
-    {id: 2, name: 'SamSung S9', price: 1200},
-    {id: 3, name: 'Xiaomi LTE9', price: 500},
-    {id: 4, name: 'Iphone 16', price: 1800}
+  { id: 1, name: 'Iphone 8', price: 700 },
+  { id: 2, name: 'SamSung S9', price: 1200 },
+  { id: 3, name: 'Xiaomi LTE9', price: 500 },
+  { id: 4, name: 'Iphone 16', price: 1800 }
 ];
 
 // Hoàn thiện hàm myFilter
@@ -16,6 +16,31 @@ var products = [
 
 function myFilter(callback, array) {
   // code logic here
+  if (typeof callback !== 'function') {
+    console.log('Tham số callback phải là 1 callback function');
+  }
+
+  if (!array || !Array.isArray(array)) {
+    console.log('Khong phai là 1 mang');
+  }
+
+  var temp = [];
+  for (var index = 0; index < array.length; index++) {
+    var value = array[index];
+    if (value) {
+      var data = callback(value, index);
+      if (value.price > 1000)
+        temp.push(data);
+    }
+  }
+  return temp;
 }
 
 // Code logic show data...
+var productsFilter = myFilter(function (product, index) {
+  return `${product.id}, ${product.name} ($${product.price})`;
+}, products);
+
+for (var productFilter of productsFilter) {
+  console.log(productFilter);
+}
